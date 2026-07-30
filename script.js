9b39c0fd339d7e55c1ca6234c3912a6a446dc7b4
Christian Savino <32826906+ChristianSavino@users.noreply.github.com>
2026-07-30 10:38:58 -0300

    Implementation card style

---
+++ script.js
@@ -0,0 +1,63 @@
+fetch("menu.json")
+.then(r=>r.json())
+.then(menu=>{
+
+    const root=document.getElementById("menu");
+
+    Object.entries(menu).forEach(([categoria,bebidas])=>{
+
+        const section=document.createElement("section");
+
+        section.className="category";
+
+        section.innerHTML=`
+
+            <div class="category-header">
+
+                <div class="category-title">${categoria}</div>
+
+                <div class="arrow">
+
+                    <svg viewBox="0 0 24 24">
+
+                        <path d="M7 10l5 5 5-5"/>
+
+                    </svg>
+
+                </div>
+
+            </div>
+
+            <div class="items"></div>
+
+        `;
+
+        const items=section.querySelector(".items");
+
+        bebidas.forEach(b=>{
+
+            items.innerHTML+=`
+
+                <div class="item">
+
+                    <h3>${b.nombre}</h3>
+
+                    ${b.descripcion ? `<p>${b.descripcion}</p>` : ""}
+
+                </div>
+
+            `;
+
+        });
+
+        section.querySelector(".category-header").onclick=()=>{
+
+            section.classList.toggle("open");
+
+        };
+
+        root.append(section);
+
+    });
+
+});
